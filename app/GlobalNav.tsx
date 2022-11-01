d4b5a8f254a695e73295ed25069f9eea2bb08435
danielP <daniel.horsens@gmail.com>
2022-11-01 15:11:37 +0100

    Update GlobalNav.tsx

--- app/GlobalNav.tsx
+++ app/GlobalNav.tsx
@@ -2,7 +2,6 @@
 
 import { demos } from '@/lib/demos';
 import clsx from 'clsx';
-import { useSelectedLayoutSegments } from 'next/navigation';
 import Link from 'next/link';
 
 import { Fragment } from 'react'
@@ -73,8 +72,6 @@ function classNames(...classes: string[]) {
 }
 
 export default function GlobalNav() {
-  const [selectedLayoutSegments] = useSelectedLayoutSegments();
-
 
   return (
     <Popover className="relative bg-white">
