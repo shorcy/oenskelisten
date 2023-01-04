a2d0a50cdab5088bb8e55d854577d7e78d07ad9b
danielP <daniel.horsens@gmail.com>
2023-01-04 22:24:31 +0100

    Seo optimize frontpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@components/HeroSection";
 
 function index() {
   return (
-    <HeroSection newsMessage="I gang med version 0.1.0" title="Din fortrukkende ønskeliste online" subTitle="Vær blandt en af de første til at få besked når vi går live. Med den spritnye ønskeliste til lige præcis dit behov." />
+    <HeroSection newsMessage="I gang med version 0.1.0" title="Din fortrukkende ønskeliste online" subTitle="Vær blandt en af de første til at få besked når ønskelisten går live. Med den spritnye ønskeliste til lige præcis dit behov." />
   )
 }
 
