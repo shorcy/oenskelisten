cc6c4f452bccace12a8e6ac996d5860b5f27498e
danielP <daniel.horsens@gmail.com>
2023-01-07 14:46:59 +0100

    Added cookie consent - cookiebot

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,48 +1,80 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang="da">
-                <Head>
-                <link rel="shortcut icon" href="/favicon.ico" />
-                    {/* Primary Meta Tags */}
-                    <title>Ønskelisten | Din fortrukkende ønskeseddel online</title>
-                    <meta name="title" content="Ønskelisten | Din fortrukkende ønskeseddel online" />
-                    <meta name="description" content="Opret din egen ønskeliste på vores platform. Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver." />
-                    
-                    {/* Open Graph / Facebook */}
-                    <meta property="og:type" content="website" />
-                    <meta property="og:url" content="https://oenskelisten.dk" />
-                    <meta property="og:title" content="Ønskelisten | Din fortrukkende ønskeseddel online" />
-                    <meta property="og:description" content="Opret din egen ønskeliste på vores platform. Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver." />
-                    <meta property="og:image" content="https://oenskelisten.dk/dashboard.webp" />
+  render() {
+    return (
+      <Html lang="da">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+          {/* Primary Meta Tags */}
+          <title>Ønskelisten | Din fortrukkende ønskeseddel online</title>
+          <meta
+            name="title"
+            content="Ønskelisten | Din fortrukkende ønskeseddel online"
+          />
+          <meta
+            name="description"
+            content="Opret din egen ønskeliste på vores platform. Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver."
+          />
 
-                    {/* Twitter */}
-                    <meta property="twitter:card" content="summary_large_image" />
-                    <meta property="twitter:url" content="https://oenskelisten.dk" />
-                    <meta property="twitter:title" content="Ønskelisten | Din fortrukkende ønskeseddel online" />
-                    <meta property="twitter:description" content="Opret din egen ønskeliste på vores platform. Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver." />
-                    <meta property="twitter:image" content="https://oenskelisten.dk/dashboard.webp" />
-                    {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
-                    
-                    {/* <meta property="og:image" content="/og-image.jpg" />
-                    <meta property="og:image:type" content="image/jpg" />
-                    <meta property="og:image:width" content="300" />
-                    <meta property="og:image:height" content="300" /> */}
-                    <meta property='og:title' content="Ønskelisten | Din fortrukkende ønskeseddel online" />
-                    <meta property='og:description' content="Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver." />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                    />
-                
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://oenskelisten.dk" />
+          <meta
+            property="og:title"
+            content="Ønskelisten | Din fortrukkende ønskeseddel online"
+          />
+          <meta
+            property="og:description"
+            content="Opret din egen ønskeliste på vores platform. Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver."
+          />
+          <meta
+            property="og:image"
+            content="https://oenskelisten.dk/dashboard.webp"
+          />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://oenskelisten.dk" />
+          <meta
+            property="twitter:title"
+            content="Ønskelisten | Din fortrukkende ønskeseddel online"
+          />
+          <meta
+            property="twitter:description"
+            content="Opret din egen ønskeliste på vores platform. Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver."
+          />
+          <meta
+            property="twitter:image"
+            content="https://oenskelisten.dk/dashboard.webp"
+          />
+          {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
+
+          <meta
+            property="og:title"
+            content="Ønskelisten | Din fortrukkende ønskeseddel online"
+          />
+          <meta
+            property="og:description"
+            content="Del den med familie og venner, så de ved præcis, hvad du ønsker dig til dit næste bryllup, fødselsdag eller jul. Vores platform gør det nemt for dig at holde styr på dine ønsker og for dem at få inspiration til gaver."
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <script
+            id="Cookiebot"
+            src="https://consent.cookiebot.com/uc.js"
+            data-cbid="13b835c1-b261-4ced-9ec6-4deef0be946e"
+            data-blockingmode="auto"
+            type="text/javascript"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import HeroSection from "@components/HeroSection";
+import HeroSection from '~/components/HeroSection';
 
 function index() {
   return (
-    <HeroSection newsMessage="I gang med version 0.1.0" title="Din fortrukkende ønskeliste online" subTitle="Vær blandt en af de første til at få besked når ønskelisten går live. Med den spritnye ønskeliste til lige præcis dit behov." />
-  )
+    <>
+      <HeroSection
+        newsMessage="I gang med version 0.1.0"
+        title="Din fortrukkende ønskeliste online"
+        subTitle="Vær blandt en af de første til at få besked når ønskelisten går live. Med den spritnye ønskeliste til lige præcis dit behov."
+      />
+      <p className='bg-'></p>
+    </>
+  );
 }
 
-export default index
+export default index;
